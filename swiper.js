833e91be260f52771136416804b0e6a5062500e4
miha04635 <124861244+miha04635@users.noreply.github.com>
2023-03-31 13:16:17 +0500

    Update swiper.js

--- swiper.js
+++ swiper.js
@@ -5,8 +5,7 @@ const screenHeight = window.screen.height;
 let mql = window.matchMedia('(max-width: 767px)');
 
 window.addEventListener('resize', () => {
-
-
+  
 let swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     spaceBetween: 30,
