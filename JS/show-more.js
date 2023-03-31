831765dd99e67d6258040ca348050815ff561bed
miha04635 <124861244+miha04635@users.noreply.github.com>
2023-03-31 11:10:27 +0500

    1

--- JS/show-more.js
+++ JS/show-more.js
@@ -1,23 +1,23 @@
 const container = document.querySelector('.swiper');
 const showMore = document.querySelector('.more');
 const bthOpen = document.querySelector('.more__button--open');
-const bthclosed = document.querySelector('.more__button--closed');
+
 const icon = document.querySelector('.icon');
 
 bthOpen.addEventListener('click', () => {
 	icon.classList.add('icon__content');
 	container.classList.add('container__height');
-	bthOpen.style.display = 'none';
 	bthclosed.style.display = 'block'
+	bthOpen.textContent = 'Скрыть';
 });
 
-bthclosed.addEventListener('click', () => {
-	container.classList.remove('container__height');
-	icon.classList.remove('icon__content');
-	bthOpen.style.display = 'block';
-	bthclosed.style.display = 'none'
+// bthclosed.addEventListener('click', () => {
+// 	container.classList.remove('container__height');
+// 	icon.classList.remove('icon__content');
+// 	bthOpen.style.display = 'block';
+// 	bthclosed.style.display = 'none'
 
-});
+// });
 
 
 
