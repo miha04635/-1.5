6f382f658a17b6bd1c9dc96673a0297fbc6978e7
miha04635 <124861244+miha04635@users.noreply.github.com>
2023-04-04 23:11:15 +0500

    1

--- show-more.js
+++ show-more.js
@@ -1,26 +1,3 @@
-// const container = document.querySelector('.swiper');
-// const showMore = document.querySelector('.more');
-// const showMoreButton = document.querySelector('.more__button');
-
-// const icon = document.querySelector('.icon');
-
-// showMoreButton.addEventListener('click', () => {
-	
-// 	icon.classList.add('icon__content');
-// 	container.classList.add('container__height');
-// 	showMoreButton.textContent = 'Скрыть';
-// });
-
-// if ( showMoreButton.textContent === 'Cкрыть') {
-// 	console.log('1')
-// 	showMoreButton.addEventListener('click', () => {
-// 	icon.classList.remove('icon__content');
-// 	container.classList.remove('container__height');
-// 	showMoreButton.textContent = 'Показать всё';
-// });
-// }
-
-
 function myFunction() {
 	let container = document.querySelector('.swiper');
 	let showMoreButton = document.querySelector('.more__button');
